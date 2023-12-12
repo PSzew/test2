55d0d114ec9ba1f26539b778a8b459b461d5e4f3
Paweł Szewczyk <Pawel.Szewczyk@zstio.edu.pl>
2023-12-12 13:18:04 +0100

    backend

---
+++ test2/test2/ClassModel/GlobalClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test2.ClassModel
+{
+    static class GlobalClass
+    {
+        static public List<Account> accounts = new List<Account>();
+    }
+}
--- test2/test2/MainPage.xaml.cs
+++ test2/test2/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using test2.ClassModel;
 using Xamarin.Forms;
 
 namespace test2
@@ -13,6 +14,17 @@ namespace test2
         public MainPage()
         {
             InitializeComponent();
+            GlobalClass.accounts.Add(new Account("loginesa", "123", new UserClass("4TP", @"https://zstio.edu.pl/plan/u/podzgodz/plan.php?kl=123F69DA832927A6")));
+            GlobalClass.accounts.Add(new Account("loginesa2", "1234", new UserClass("2TP", @"https://zstio.edu.pl/plan/u/podzgodz/plan.php?kl=3A4D87253A6AAA3F")));
+            GlobalClass.accounts.Add(new Account("loginesa3", "12345", new UserClass("5TP", @"https://zstio.edu.pl/plan/u/podzgodz/plan.php?kl=2FE6DBDB64885790")));
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            foreach (var account in GlobalClass.accounts) 
+            {
+               
+            }
         }
     }
 }
